fd4032ec803361db703bbd003ff8b4d428370423
hiyouhow <153273601+hiyouhow@users.noreply.github.com>
2024-01-02 23:48:18 +0800

    Update playmove.cs

--- Assets/Scenes/playmove.cs
+++ Assets/Scenes/playmove.cs
@@ -13,27 +13,60 @@ public class playmove : MonoBehaviour
     public Transform groundcheck;
     public float checkradius;
     public LayerMask whatisground;
-    
-    
+    private bool candash=true;
+    private bool isdashing;
+    public float dashspeed;
+    public float dashcd=1f;
+
+    private float dashtime=0.2f;
+
+    private int m;
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        this.rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {   
+        if(isdashing){
+            return;
+        }
         isgrounded=Physics2D.OverlapCircle(groundcheck.position,checkradius,whatisground);
 
         moveinput=Input.GetAxis("Horizontal");
         Debug.Log(moveinput);
-        rb.velocity= new Vector2(moveinput*speed,rb.velocity.y);
+        this.rb.velocity= new Vector2(moveinput*speed,this.rb.velocity.y);
     }
      void Update() {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity=Vector2.up*jumpforce;
+        if(Input.GetKeyDown(KeyCode.Space)&&isgrounded==true){
+            this.rb.velocity=Vector2.up*jumpforce;
         }
+        if(Input.GetKeyDown(KeyCode.LeftShift)&&candash==true){
+            StartCoroutine(dash());
+
+        }
+
+    }
+    private IEnumerator dash(){
+        m=0;
+        if(moveinput<0){
+            m=-1;
+        }
+        else if(moveinput>0){
+            m=1;
+        }
+        candash=false;
+        isdashing=true;
+        float origrav=rb.gravityScale;
+        rb.gravityScale=0f;
+        rb.velocity=new Vector2(transform.localScale.x*dashspeed*m,0f);
+        yield return new WaitForSeconds(dashtime);
+        rb.gravityScale=origrav;
+        isdashing=false;
+        yield return new WaitForSeconds(dashcd);
+        candash=true;
 
     }
 }
